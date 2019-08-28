3ffbb6597ea7730515f1dbdd96d21895905c4697
Andrey Naligatski Dias <43612323+Dreyyyyy@users.noreply.github.com>
2019-08-28 14:17:37 -0300

    Falsa posição

---
+++ Exercicio_28_09/falsaposicao2058880.m
@@ -0,0 +1,28 @@
+function [x k] = falsaposicao2058880(f, a, b)
+  Nmax = 200;
+  epsilon = 10.^(-4);
+  k = 1;
+  if f(a) * f(b) > 0
+    x = -1;
+    k = 0;
+    return;
+  endif
+  for i = 0: Nmax
+    x = (a * f(b) - b * f(a))/ (f(b) - f(a));
+    if f(a) * f(x) < 0
+      b = x;
+    else
+      a = x;
+    endif  
+    if abs(b - a) < epsilon
+      return;
+    endif
+    if abs(f(x)) < epsilon
+      return;
+    endif
+    if k > Nmax
+      return;
+    endif
+    k++;
+  endfor 
+endfunction
