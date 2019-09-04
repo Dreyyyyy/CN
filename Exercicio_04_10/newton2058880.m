f94e4fd2ce69b6a200b92b41571213367d2323f2
Andrey Naligatski Dias <43612323+Dreyyyyy@users.noreply.github.com>
2019-09-04 12:37:19 -0300

    Função de newton

---
+++ Exercicio_04_10/newton2058880.m
@@ -0,0 +1,29 @@
+function x = newton2058880(f, df,x0)
+  tol = 1e-4;
+  nMax = 500;
+  if x0 == 0
+    x = x0;
+    return;
+  endif
+  for i = 0: nMax
+    if i >= 1
+      xAnt = x;  
+    else
+      xAnt = x0;
+    endif 
+    if df(xAnt) == 0
+      x = -1;
+      return;
+    endif  
+    x = xAnt - f(xAnt)/ df(xAnt);
+    if abs(x - xAnt)/abs(x) < tol
+      return;
+    endif
+    if abs(f(x))/abs(df(xAnt)) < tol
+      return;
+    endif
+    if i > nMax
+      return;
+    endif
+  endfor
+endfunction
